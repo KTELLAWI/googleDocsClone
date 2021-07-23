9e4e6bcdd9b37f7fe3954284b0ad2c4771c0262b
KTELLAWI <koutaiba@msn.com>
2021-07-24 01:27:44 +0300

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
+import '../style.css'
+
+import "@material-tailwind/react/tailwind.css";
+import {Provider} from 'next-auth/client'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Head>
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+/>
+
+  </Head>
+  <Provider session ={pageProps.session}>
+  <Component {...pageProps} />
+  </Provider>
+  </>
+  )
 }
 
 export default MyApp
